de499db28ee7bfee0865a33e3a17b74348f19b2b
Tho <tho@Thos-MacBook-Pro.local>
2023-06-14 10:10:18 +0700

    ios app

---
+++ log/log/SceneDelegate.swift
@@ -0,0 +1,106 @@
+//
+//  SceneDelegate.swift
+//  log
+//
+//  Created by Tho on 14/06/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+        let webSocketManager = WebSocketManager()
+        webSocketManager.connectToWebSocket()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            // Usage example
+            // Send log message
+            [0,1,2,3,4,5].forEach { _ in
+                webSocketManager.sendLogMessage(message: "logMessage : \(Date())")}
+            
+        })
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+
+}
+
+import Foundation
+
+class WebSocketManager {
+    var webSocketTask: URLSessionWebSocketTask?
+
+    func connectToWebSocket() {
+        let url = URL(string: "ws://192.168.1.3:9999")!
+        let session = URLSession(configuration: .default)
+        webSocketTask = session.webSocketTask(with: url)
+        webSocketTask?.resume()
+
+        receiveMessages()
+    }
+
+    func sendLogMessage(message: String) {
+        let message = URLSessionWebSocketTask.Message.string(message)
+        webSocketTask?.send(message) { error in
+            if let error = error {
+                print("Error sending WebSocket message: \(error)")
+            } else {
+                print("Log message sent successfully")
+            }
+        }
+    }
+
+    func receiveMessages() {
+        webSocketTask?.receive { [weak self] result in
+            switch result {
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    print("Received message from WebSocket server: \(text)")
+                case .data(let data):
+                    print("Received data from WebSocket server: \(data)")
+                }
+            case .failure(let error):
+                print("Error receiving WebSocket message: \(error)")
+            }
+
+            self?.receiveMessages() // Continuously receive messages
+        }
+    }
+}
+
+
---
+++ log/log/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  log
+//
+//  Created by Tho on 14/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
