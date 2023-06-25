68c60ca7cec0ebe4aabc8a701abceff13170f721
thond02 <thond02@ominext.com>
2023-06-25 16:31:26 +0700

    refactor socket.js

---
+++ socket.js
@@ -0,0 +1,68 @@
+const socket = new WebSocket('ws://localhost:9999');
+
+// WebSocket code here...
+function showMessageDetail(message) {
+  const messageText = document.getElementById('message-text');
+  messageText.textContent = message;
+}
+socket.onmessage = function (event) {
+  const message = event.data;
+  const timestamp = new Date().toLocaleTimeString(); // Get the current time
+
+  // Determine the message type
+  const messageType = message.startsWith('[ERROR]') ? 'error' : 'debug';
+
+  // Create message row with timestamp and message type
+  const messageRow = document.createElement('div');
+  messageRow.className = `message-row ${messageType}`;
+  messageRow.innerHTML = `<span class="timestamp ${messageType}">[${timestamp}]</span> ${message}`;
+
+  // Add click event listener to show full message in detail panel
+  messageRow.addEventListener('click', function () {
+    const messageDetail = document.getElementById('message-detail');
+    messageDetail.innerHTML = '';
+
+    // Remove the active class from all rows
+    const messageRows = document.getElementsByClassName('message-row');
+    for (let i = 0; i < messageRows.length; i++) {
+      messageRows[i].classList.remove('active');
+    }
+
+    // Toggle the active class on the selected row
+    messageRow.classList.toggle('active');
+
+    // Determine the content type and format accordingly
+    if (message.startsWith('<') && message.endsWith('>')) {
+      // XML content
+      messageDetail.textContent = message;
+    } else if (message.startsWith('{') && message.endsWith('}')) {
+      // JSON content
+      const formattedJSON = prettyPrintJson.toHtml(message);
+      messageDetail.innerHTML = formattedJSON;
+    } else {
+      // Plain text content
+      messageDetail.innerHTML = message;;
+    }
+  });
+
+  const messageList = document.getElementById('message-list');
+
+  // Insert message row at the beginning of message list container
+  messageList.prepend(messageRow);
+};
+
+function filterMessages() {
+  const searchText = document.getElementById('search-text').value.toLowerCase();
+  const messages = document.getElementsByClassName('message-row');
+
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    const messageText = message.textContent.toLowerCase();
+
+    if (messageText.includes(searchText)) {
+      message.style.display = 'block';
+    } else {
+      message.style.display = 'none';
+    }
+  }
+}
